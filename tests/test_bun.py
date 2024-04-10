18e99e9a288769fd2935b3e331d58eb00004bde0
Julia Sitnikova <sitnikova23@yandex.ru>
2024-04-10 18:34:20 +0300

    First commit

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -2,16 +2,17 @@ import random
 
 from praktikum.database import Database
 
-def test_get_bun_return_name():
-    db = Database()
-    bun = random.choice(db.available_buns())
-    bun_name_from_db = bun.name
-    bun_name = bun.get_name()
-    assert bun_name == bun_name_from_db
+class TestBun:
+    def test_get_bun_return_name(self):
+        db = Database()
+        bun = random.choice(db.available_buns())
+        bun_name_from_db = bun.name
+        bun_name = bun.get_name()
+        assert bun_name == bun_name_from_db
 
-def test_get_bun_return_price():
-    db = Database()
-    bun = random.choice(db.available_buns())
-    bun_price_from_db = bun.price
-    bun_price = bun.get_price()
-    assert bun_price == bun_price_from_db
+    def test_get_bun_return_price(self):
+        db = Database()
+        bun = random.choice(db.available_buns())
+        bun_price_from_db = bun.price
+        bun_price = bun.get_price()
+        assert bun_price == bun_price_from_db
